2a91d905deac520efa201d907d406a7862632b19
Andrés Felipe Salamanca Silva <56135223+MRPinas2100@users.noreply.github.com>
2024-10-21 19:29:21 -0500

    First panel component for weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,102 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      {/* search section */}
+      <section className="search-section">
+        <form action="#" className="search-form">
+          <span className="material-symbols-outlined">search</span>
+          <input
+            type="search"
+            className="search-input"
+            placeholder="Enter a city name"
+            required
+          />
+        </form>
+        <button className="location-btn">
+          <span className="material-symbols-outlined">my_location</span>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </section>
+
+      {/* weather section */}
+      <section className="weather-section">
+        <div className="current-weather">
+          <img
+            src="icons/clouds.svg"
+            alt="clouds icon"
+            className="weather-icon"
+          />
+          <h2 className="temperature">
+            20 <span>°C</span>
+          </h2>
+          <p className="description">Partly Cloudy</p>
+        </div>
+
+        {/* Hourly weather forecastlist */}
+        <div className="hourly-forecast">
+          <ul className="weather-list">
+            <li className="weather-item">
+              <p className="time">00:00</p>
+              <img
+                src="icons/clouds.svg"
+                alt="clouds icon"
+                className="weather-icon"
+              />
+              <p className="temperature">20°</p>
+            </li>
+
+            <li className="weather-item">
+              <p className="time">00:00</p>
+              <img
+                src="icons/clouds.svg"
+                alt="clouds icon"
+                className="weather-icon"
+              />
+              <p className="temperature">20°</p>
+            </li>
+
+            <li className="weather-item">
+              <p className="time">00:00</p>
+              <img
+                src="icons/clouds.svg"
+                alt="clouds icon"
+                className="weather-icon"
+              />
+              <p className="temperature">20°</p>
+            </li>
+
+            <li className="weather-item">
+              <p className="time">00:00</p>
+              <img
+                src="icons/clouds.svg"
+                alt="clouds icon"
+                className="weather-icon"
+              />
+              <p className="temperature">20°</p>
+            </li>
+
+            <li className="weather-item">
+              <p className="time">00:00</p>
+              <img
+                src="icons/clouds.svg"
+                alt="clouds icon"
+                className="weather-icon"
+              />
+              <p className="temperature">20°</p>
+            </li>
+
+            <li className="weather-item">
+              <p className="time">00:00</p>
+              <img
+                src="icons/clouds.svg"
+                alt="clouds icon"
+                className="weather-icon"
+              />
+              <p className="temperature">20°</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
   )
 }
 
